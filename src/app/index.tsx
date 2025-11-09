cf4ade7f9beb7459c2ed1232221c1a2579811891
Anderson Santo <andersonsanto08@gmail.com>
2025-11-09 17:43:52 -0300

    feat: add TargetItem component and integrate it into index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import { View } from "react-native";
 
 import { HomeHeader } from "@/components/HomeHeader";
+import { TargetItem } from "@/components/TargetItem";
 
 const summaryData = {
   total: "R$ 2.680,00",
@@ -8,10 +9,21 @@ const summaryData = {
   output: { label: "Saídas", value: "-R$ 883,65" },
 };
 
+const targets = [
+  {
+    name: "Comprar uma cadeira ergonômica",
+    percentage: "75%",
+    current: "900,00",
+    target: "1.200,00",
+  },
+];
+
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
       <HomeHeader data={summaryData} />
+
+      <TargetItem data={targets[0]} />
     </View>
   );
 }
---
+++ src/components/TargetItem/styles.ts
@@ -0,0 +1,27 @@
+import { colors, fonts } from "@/theme";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    height: 72,
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+    paddingBottom: 16,
+  },
+  content: {
+    flex: 1,
+    gap: 7,
+  },
+  name: {
+    fontSize: 14,
+    color: colors.black,
+    fontFamily: fonts.medium,
+  },
+  status: {
+    fontSize: 10,
+    color: colors.gray[500],
+    fontFamily: fonts.regular,
+  },
+});
